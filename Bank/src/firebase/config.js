2b7e46b0f9b21124617a7274b8210e502d1ab285
Prajwal P Shetty <prajwalpshetty@Prajwals-MacBook-Air.local>
2025-01-22 16:55:18 +0530

    deploy

--- Bank/src/firebase/config.js
+++ Bank/src/firebase/config.js
@@ -16,4 +16,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
-export const db = getFirestore(app);
+export const db = getFirestore(app);//commit
